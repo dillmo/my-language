ec97eb8d1a38b3412eb94f85f3ce9dc1ac332540
Dillon Morse <dillon@inoffensiveandwarm.com>
2020-06-24 09:08:18 -0400

    basic grammar with only one identifier

--- generator/generator.c
+++ generator/generator.c
@@ -1,4 +1,27 @@
+#include <stdio.h>
+
+char *program();
+char *identifier();
+
 int main(void)
 {
+  printf("%s\n", program());
+
   return 0;
+}
+
+/*
+ * <program> -> <identifier>
+ */
+char *program()
+{
+  return identifier();
+}
+
+/*
+ * <identifier> -> a
+ */
+char *identifier()
+{
+  return "a";
 }
